4c85f0764b596d42c6ed03f718155fb5e9c760ce
Yanis Mercier-Tallet <yanis.mercier-tallet@etu.u-bordeaux.fr>
2023-01-05 09:02:30 +0100

    ajout Pays et date d'inscription

--- SAE_S3.01/src/Controller/RegistrationController.php
+++ SAE_S3.01/src/Controller/RegistrationController.php
@@ -31,7 +31,8 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
-
+            $user->setRegisterDate(new \DateTime('@'.strtotime('Europe/Paris')));
+//$user->setRegisterDate(new \DateTime(new \DateTimeZone('Europe/Paris')));
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
--- SAE_S3.01/src/Form/RegistrationFormType.php
+++ SAE_S3.01/src/Form/RegistrationFormType.php
@@ -44,7 +44,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            
+            ->add('country')            
         ;
     }
 
