57687a0a8fd0b52bfda6c6f24a101795e9809e11
Baptiste Laborde <baptiste.laborde@etu.u-bordeaux.fr>
2023-01-06 13:37:46 +0100

    Affichage posters aléatoires

--- SAE_S3.01/src/Controller/DefaultController.php
+++ SAE_S3.01/src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Series;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_default')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        /* TODO: Générer 3 nombres randoms, récupérer les séries
+        correspondantes dans $series et les envoyer dans le return. */
+
+        $series = $entityManager
+            ->getRepository(Series::class)
+            ->findBy([], ['title' => 'ASC']);
+
+        $a = rand(0,sizeof($series));
+        $b = rand(0,sizeof($series));
+        $c = rand(0,sizeof($series));
+
+        $a = $series[$a];
+        $b = $series[$b];
+        $c = $series[$c];
+
+        $series = [$a, $b, $c];
+
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
+            'series' => $series,
         ]);
     }
 }
