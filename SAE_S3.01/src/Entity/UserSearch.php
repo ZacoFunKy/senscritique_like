7689f2ce7e221f47af082003dbc11bf65fb42bcf
Lucas Jacquemard <lucas.jacquemard@etu.u-bordeaux.fr>
2023-01-17 15:30:04 +0100

    Ajout de la documentation dans user et dans user create et user search

--- SAE_S3.01/src/Entity/UserSearch.php
+++ SAE_S3.01/src/Entity/UserSearch.php
@@ -7,12 +7,23 @@ class UserSearch
 
    private $nom;
 
-   
+    /**
+     * Permet d'obtenir le nom de l'utilisateur dans la barre de recherche
+     *
+     * @return ?string
+     */
    public function getNom(): ?string
    {
        return $this->nom;
    }
 
+   /**
+     * Permet d'obtenir le nom de l'utilisateur dans le formulaire
+     *
+     * @param string $nom le nom de l'utilisateur dans la barre de recherche
+     *
+     * @return self
+     */
    public function setNom(string $nom): self
    {
        $this->nom = $nom;
