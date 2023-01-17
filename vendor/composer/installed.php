ea49928f6c59da07032f550e62cc57a2574470f4
Florian Berger <florian.berger@etu.u-bordeaux.fr>
2023-01-17 10:04:29 +0100

    ajout MCD final

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ff363e8ea0fcba78e8df1cdbf115095be8e6717c',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ff363e8ea0fcba78e8df1cdbf115095be8e6717c',
+            'dev_requirement' => false,
+        ),
+        'symfony/runtime' => array(
+            'pretty_version' => 'v6.2.0',
+            'version' => '6.2.0.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../symfony/runtime',
+            'aliases' => array(),
+            'reference' => '45a63a5885f92741c8def9cbd81c925e6b5b891d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
